c622226ffe0f288b6d64c2d57502b79dc791c162
Dirk Dorneich <dfamilyacc@gmail.com>
2019-02-20 21:24:37 -0500

    __test__ disable eslint

--- __tests__/App.js
+++ __tests__/App.js
@@ -8,8 +8,10 @@ import React from 'react';
 import App from '../App';
 
 // Note: test renderer must be required after react-native.
+// eslint-disable-next-line import/order
 import renderer from 'react-test-renderer';
 
+// eslint-disable-next-line no-undef
 it('renders correctly', () => {
   renderer.create(<App />);
 });
